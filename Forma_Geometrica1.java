820cdd9f3dcf17a80a33372e3b310bb58487d280
Caio <78882269+caioluscas@users.noreply.github.com>
2022-07-19 20:09:47 -0300

    Add files via upload

---
+++ Forma_Geometrica1.java
@@ -0,0 +1,46 @@
+
+public class Forma_Geometrica1 {
+	int raio;
+	double pi;
+	String cor;
+	
+	public Forma_Geometrica1(int raio, double pi, String cor) {
+		super();
+		this.raio = raio;
+		this.pi = pi;
+		this.cor = cor;
+	}
+
+	public int getRaio() {
+		return raio;
+	}
+
+	public void setRaio(int raio) {
+		this.raio = raio;
+	}
+
+	public double getPi() {
+		return pi;
+	}
+
+	public void setPi(double pi) {
+		this.pi = pi;
+	}
+
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+	
+	
+
+	
+	
+	
+	
+	
+
+}
---
+++ Teste1.java
@@ -0,0 +1,13 @@
+
+public class Teste1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Circulo1 ci = new Circulo1(6, 3.14, "Azul");
+		ci.Area();
+		System.out.print("Area: "+ci.Area()+" cor:");
+		ci.Cor();
+
+	}
+
+}
